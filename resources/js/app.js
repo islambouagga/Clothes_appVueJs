7a8f9e6496c202fe7405ba4a55b36b4571f105e1
islambouagga <44034848+islambouagga@users.noreply.github.com>
2021-02-05 16:04:17 +0100

    customz clothes card

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,16 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
--- resources/js/components/Clothes.vue
+++ resources/js/components/Clothes.vue
@@ -1,16 +1,117 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Clothes Component</div>
+        <div class="row d-flex align-items-stretch mt-5">
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
 
-                    <div class="card-body">
-                        I'm an Clothes component.
-                    </div>
-                </div>
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
             </div>
-        </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-card-text>
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </div>
+            </div>
+
     </div>
 </template>
 
